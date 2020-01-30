6e300e72b8b2b1baa48a3c31915ce05caf2d487a
ezreum <whismaster_power@hotmail.com>
2020-01-30 13:56:18 +0100

    todo

--- Jquery/ejercicio1/js/ejer1.js
+++ Jquery/ejercicio1/js/ejer1.js
@@ -4,20 +4,31 @@ $(document).ready(function() {
     var $cont= $("div.module");
 
     var $tercerLista = $("#myList").children().eq(2);
-    var $tercerLista1 = $("#mylist").childen().has();
-    var $tercerLista2 = ;
+    var $tercerLista1 = $("#myList").children().first().next().next();
+    var $tercerLista2 = $("#myList").find("li").eq(2);
+
+    var $label = $("input.input_text");
+
+
 
     $cont.click(function() {
         if ( $cont.length>0 ) {
-            alert("hay "+$cont.length+" divs");
+            alert("hay "+$cont.length+" divs de clase modulo");
         }
     }) 
 
-    $tercerLista.click(function() {
-        if ( $tercerLista.length>0 ) {
-            alert("hay "+$tercerLista.length+" elementos");
+    $tercerLista2.click(function() {
+        if ( $tercerLista2.length>0 ) {
+            alert("hay "+$tercerLista2.length+" elementos");
         }
     }) 
 
+    $label.closest("label").on("click", function() {
+        alert($label.closest("label").val());
+    })
 
 })
+
+
+
+

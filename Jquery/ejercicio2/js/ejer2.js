ca43efef58d1379f6a1d68126e08e89de52d1317
ezreum <whismaster_power@hotmail.com>
2020-02-03 14:10:00 +0100

    todo

--- Jquery/ejercicio2/js/ejer2.js
+++ Jquery/ejercicio2/js/ejer2.js
@@ -1,43 +1,21 @@
 $(document).ready(function() {
     
-    //por convención a las variables que encierran un objeto se suelen tratar con un dolar delante
-    var $cont= $("div.module");
+    //var alts = $("[alt]");
 
-    var $tercerLista = $("#myList").children().eq(2);
-    var $tercerLista1 = $("#myList").children().first().next().next();
-    var $tercerLista2 = $("#myList").find("li").eq(2);
-
-    var $input = $("input.input_text");
-
-    var $invisible=$("*:hidden");
-    alert("hay " +$invisible.length + " elementos ocultos");
-
-    var alts = $("[alt]");
-
-    var filas = $("tr:even");
+    var $alts = $("img");
+    var $entrada = $(":input");
     
-    $cont.click(function() {
-        if ( $cont.length>0 ) {
-            alert("hay "+$cont.length+" divs de clase modulo");
-        }
-    }) 
+    $alts.on("click", function() {
 
-    $tercerLista2.click(function() {
-        if ( $tercerLista2.length>0 ) {
-            alert("hay "+$tercerLista2.length+" elementos");
-        }
-    }) 
-
-    $input.parent().on("click", function() {
-        alert($input.parent().text());
+        $alts.each( function (index, value) {             
+             alert(index+" "+$(value).attr("alt"));
+             //console.log($(this).attr("alt"));    
+        });
     })
 
-    alts.on("click", function() {
-        alert("hay "+alts.length+" alts");
-    })
-
-    filas.on("click", function() {
-        alert("hay "+filas.length+" filas impares");
+    $entrada.on("click", function() {
+        
+        alert($entrada.val());
     })
     
 

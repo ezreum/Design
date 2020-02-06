cf078898ad5271a7a867b20db9109bbcebd8f1e2
ezreum <whismaster_power@hotmail.com>
2020-02-06 13:42:48 +0100

    todo

--- Jquery/ejercicio3/js/ejer3.js
+++ Jquery/ejercicio3/js/ejer3.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     
     //var alts = $("[alt]");
 
-    var $alts = $("img");
+    var $lista = $("#myList");
     var $entrada = $(":input");
     var $ahora = $(".current");
     var $siguiente = $ahora.next();
@@ -11,42 +11,29 @@ $(document).ready(function() {
 
     var $deshabil = $("#slideshow");
 
-    $alts.on("click", function() {
+    $lista.one("click", function() {
 
-        $alts.each( function (index, value) {             
-             alert(index+" "+$(value).attr("alt"));
-             //console.log($(this).attr("alt"));    
-        });
-    })
+        var element ="";
+        alert("insertando elementos");
+        for (let i = 0; i < 5; i++) {
+            element += "<li>elemento li</li>";
+        }
+        $lista.append(element);
 
-    $entrada.on("click", function() {
-        $entrada.addClass("pepe");
-        alert("el valor del campo es: "+$entrada.val());
-    })
-    
-    $ahora.one("mouseover", function () {
-        alert("la clase current pasa de "+$ahora.text()+" a "+$siguiente.text());
-        $ahora.removeClass("current");
-        $siguiente.addClass("current");
+        alert("quitando los elementos impares");
+        var t=[];
+        for (let e = 0; e < $lista.children().length; e++) {
+            e%2==1?'':t.push(e);
+        }
+
+        for (let index = t.length-1; index >= 0 ; index--) {
+            $lista.children().eq(t[index]).remove();
+        }
+
     });
 
-    $especialito.on("click", function () {
-        if ($especialito.find("select") instanceof jQuery) {
-            alert("hooooola soy el boton dentro del form y mi valor es: "+$especialito.find("select").parent().next().children().eq(0).val());
-        }
-        else{
-            alert("ha ha");
-        }
-        
-    });
 
-    $deshabil.one("click", function () {
-        alert("deshabilitando todos los li excepto "+ $deshabil.children().eq(0).text());
-        for (let index = 0; index < $deshabil.children().length; index++) {
-            $deshabil.children()[index] == $deshabil.children()[0]?
-            $deshabil.children().eq(index).addClass("current"):
-            $deshabil.children().eq(index).addClass("disabled");    
-        }
-    });
+
+
 
 })

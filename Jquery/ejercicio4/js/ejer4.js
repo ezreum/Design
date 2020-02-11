58c71f19ea7d5be10af4db4e8776c48b94e7654a
ezreum <whismaster_power@hotmail.com>
2020-02-11 14:11:46 +0100

    todo

--- Jquery/ejercicio4/js/ejer4.js
+++ Jquery/ejercicio4/js/ejer4.js
@@ -1,54 +1,22 @@
 $(document).ready(function() {
     
-    //var alts = $("[alt]");
-
-    var $lista = $("#myList");
-
-    var $modulo = $("div.module");
-
-    var $seleccion = $("div.module").find("select");
-
-    var divero = $("<div class=\"module\"></div>").html($("img"));
-
-    $("body").append(divero);
-
-    $lista.one("click", function() {
-
-        var element ="";
-        alert("insertando elementos");
-        for (let i = 0; i < 5; i++) {
-            element += "<li>elemento li</li>";
-        }
-        $lista.append(element);
-
-        alert("quitando los elementos impares");
-        var t=[];
-        for (let e = 0; e < $lista.children().length; e++) {
-            e%2==1?'':t.push(e);
-        }
-
-        for (let index = t.length-1; index >= 0 ; index--) {
-            $lista.children().eq(t[index]).remove();
+    $("html").on("click",function(e) {
+        if (!e.target.id=='' ) {
+            $("body").css('background-color', e.target.id); 
         }
 
     });
 
-
-    $modulo.one("click", function(){
-        alert("insertando cabecera y párrafo JQUERY style");
-        var cabecera = $("<h2></h2>").text("Otro h2");
-        var p = $("<p></p>").text("Otro párrafo");
-        $modulo.append(cabecera,p);
-    });
-
-    
-
-
-    $seleccion.one("click", function(){
-        alert("insertando wednesday");
-       var $opcion = $("<option></option>").text("wednesday");
-       $seleccion.append($opcion);
-    });
+    $(".me").hover(function (e) {
+        console.log("clickó horizontalmente en: "+e.pageX+". Y verticalmente en: "+e.pageY);
+        var $color = $("<span></span>").text(e.target.id);
+        $color.css("color","grey");
+        //$color.css("position:absolute", e.pageX);
+        $("body").append($color);
+        }, function () {
+        $("body").children().last().remove();
+        }
+    );
 
 
 });

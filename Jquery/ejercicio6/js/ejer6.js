c5be63ce5926062bffc48dc093827e80c987da18
ezreum <whismaster_power@hotmail.com>
2020-02-18 10:56:02 +0100

    todo

--- Jquery/ejercicio6/js/ejer6.js
+++ Jquery/ejercicio6/js/ejer6.js
@@ -4,19 +4,19 @@ $(document).ready(function() {
     var cuadradin = cuadrado.creator();
     $todo.on("keydown", function(e){
         if (e.which==37){
-            cuadradin.x -= 1;
+            cuadradin.x -= 10;
             cuadrado.creator2(cuadradin);
         }
         else if(e.which==38){
-            cuadradin.y -= 1;
+            cuadradin.y -= 10;
             cuadrado.creator2(cuadradin);
         }
         else if(e.which==39){
-            cuadradin.x += 1;
+            cuadradin.x += 10;
             cuadrado.creator2(cuadradin);
         }
         else if(e.which==40){
-            cuadradin.y += 1;
+            cuadradin.y += 10;
             cuadrado.creator2(cuadradin);
         }
     });

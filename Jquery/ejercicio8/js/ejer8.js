94d771afb5c493efd5562dea6b6159cf3f6409de
ezreum <whismaster_power@hotmail.com>
2020-02-21 18:56:28 +0100

    todo

--- Jquery/ejercicio8/js/ejer8.js
+++ Jquery/ejercicio8/js/ejer8.js
@@ -1,9 +1,50 @@
 $(document).ready(function () {
     
     $('button').on("click", function() {
-        console.log("aqui");
-        $('#imagen').animate({
-            "backgroundSize": "175%"},1000);
+        
+        $('#imagen').animate({ backgroundSize: '175%'
+         
         })
+    })
+
+
+
+    $('.cuadrantes').hover( 
+        
+        function() {
+        if ($(this).attr("id") == "uno") {
+            $('#imagen').css('background-position','left top');
+            $('#imagen').animate({ backgroundSize: '175%'   
+        })
+        }
+
+        else if ($(this).attr("id") == "dos") {
+            $('#imagen').css('background-position','right top');
+            $('#imagen').animate({ backgroundSize: '175%'   
+        })
+        }
+
+        else if ($(this).attr("id") == "tres") {
+            $('#imagen').css('background-position','left bottom');
+            $('#imagen').animate({ backgroundSize: '175%'   
+        })
+        }
+
+        else {
+        $('#imagen').css('background-position','right bottom');
+        $('#imagen').animate({ backgroundSize: '175%'   
+            })
+        }
+    },
+
+    function() {
+        $('#imagen').finish();
+        $('#imagen').css('background-position','center');
+            $('#imagen').animate({ backgroundSize: '100%'   
+        })
+      }
+
+    );
+
 
 });

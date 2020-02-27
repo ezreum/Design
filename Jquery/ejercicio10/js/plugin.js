b41c769aecc40e9aaa9db2e47c85cb89cbb3ea5a
ezreum <whismaster_power@hotmail.com>
2020-02-27 11:55:39 +0100

    todo

--- Jquery/ejercicio10/js/plugin.js
+++ Jquery/ejercicio10/js/plugin.js
@@ -4,15 +4,13 @@
 	$(this).on("mousemove",function(e){		
 		$(".tip").text($(this).data("tip"));
 		$(".tip").css({
+			"display": "block",
 			"top": e.pageY+5,
-			"left": e.pageX+5,
-			"display": "block"	
+			"left": e.pageX+5
 		});
 	});
 	$(this).on("mouseleave", function(){
 		$(".tip").css("display","none");
 	})
-
-
 	}
 })(jQuery);
